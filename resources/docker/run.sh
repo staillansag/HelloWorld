6895f07ad8b4b1d379d1e1c58e17fea530c70c7b
staillansag <103417302+staillansag@users.noreply.github.com>
2023-08-21 22:14:53 +0200

    Adding UM

--- resources/docker/run.sh
+++ resources/docker/run.sh
@@ -3,9 +3,10 @@ CONTAINER_NAME=msrdce
 HOST_PORT=15555
 
 docker run --name $CONTAINER_NAME \
+	--network sag \
 	-d -p $HOST_PORT:5555 \
 	-v $MICROSERVICE_HOME/resources/docker/license/msr-license.xml:/opt/softwareag/IntegrationServer/config/licenseKey.xml:ro \
-	-v $MICROSERVICE_HOME/application.properties.test:/opt/softwareag/IntegrationServer/application.properties \
+	-v $MICROSERVICE_HOME/application.properties:/opt/softwareag/IntegrationServer/application.properties \
 	-v $MICROSERVICE_HOME:/opt/softwareag/IntegrationServer/packages/HelloWorld \
 	-v $MICROSERVICE_HOME/jms/jndi_DEFAULT_IS_JNDI_PROVIDER.properties:/opt/softwareag/IntegrationServer/config/jndi/jndi_DEFAULT_IS_JNDI_PROVIDER.properties \
 	-v $MICROSERVICE_HOME/jms/jms.cnf:/opt/softwareag/IntegrationServer/config/jms.cnf \
